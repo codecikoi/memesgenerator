d39a6389169003175512b597f7fed016caeade6d
Zoir I <wowdobryy@gmail.com>
2023-03-10 00:30:08 +0300

    initial

---
+++ android/app/src/main/kotlin/zoir/co/memesgenerator/MainActivity.kt
@@ -0,0 +1,6 @@
+package zoir.co.memesgenerator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/resources/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  const AppImages._();
+
+  static const ironman = 'assets/images/ironman.png';
+  static const superman = 'assets/images/superman.png';
+  static const hulk = 'assets/images/hulk.png';
+  static const unknown = 'assets/images/unknown.png';
+  static const unknownBig = 'assets/images/unknown_big.png';
+
+}
