187b1406ae33edec8f8aad7db8b637a4271d5a59
Zoir I <wowdobryy@gmail.com>
2023-03-21 20:42:22 +0300

    maked serialazible non-serialazible values

--- lib/data/models/text_with_position.dart
+++ lib/data/models/text_with_position.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:memesgenerator/data/models/position.dart';
@@ -9,11 +11,17 @@ class TextWithPosition extends Equatable {
   final String id;
   final String text;
   final Position position;
+  final double? fontSize;
+
+  @JsonKey(toJson: colorToJson, fromJson: colorFromJson)
+  final Color? color;
 
   const TextWithPosition({
     required this.id,
     required this.text,
     required this.position,
+    this.fontSize,
+    this.color,
   });
 
   factory TextWithPosition.fromJson(final Map<String, dynamic> json) =>
@@ -22,5 +30,15 @@ class TextWithPosition extends Equatable {
   Map<String, dynamic> toJson() => _$TextWithPositionToJson(this);
 
   @override
-  List<Object?> get props => [position, text, id];
+  List<Object?> get props => [position, text, id, fontSize, color];
+}
+
+String? colorToJson(final Color? color) {
+  return color == null ? null : color.value.toRadixString(16);
+}
+
+Color? colorFromJson(final String? colorString) {
+  if (colorString == null) return null;
+  final intColor = int.tryParse(colorString, radix: 16);
+  return intColor == null ? null : Color(intColor);
 }
